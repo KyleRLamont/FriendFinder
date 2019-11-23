e8dca458cd7565b99f1e94d2c87ccda93d20f798
Kyle LaMont <kylerlamont@gmail.com>
2019-11-23 13:27:45 -0800

    edited friend.js so the info is exportable

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,6 @@
-[
+var friends = require("")
+
+var frienddata = [
     {
         "name": "Ahmed",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
@@ -128,3 +130,5 @@
         ]
     }
 ]
+
+module.exports(frienddata);
